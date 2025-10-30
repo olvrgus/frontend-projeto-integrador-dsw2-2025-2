73c8480ad491f8c245fa9778f6d03d6c421a09d0
olvrgus <go0694447@gmail.com>
2025-10-30 17:21:21 -0300

    ajustando as rotas e os componentes das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,26 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./pages/Discos.jsx";
+import App from "./pages/discos/DiscosShow.jsx";
 import Sobre from "./pages/Sobre.jsx";
 import Contato from "./pages/Contato.jsx";
-import Discos from "./pages/Discos.jsx";
-import DiscosCreate from "./pages/DiscosCreate.jsx";
-import DiscosEdit from "./pages/DiscosEdit.jsx";
+import DiscosShow  from "./pages/discos/DiscosShow.jsx";
+import DiscosCreate from "./pages/discos/DiscosCreate.jsx";
+import DiscosEdit from "./pages/discos/DiscosEdit.jsx";
+import DiscosIndex from "./pages/discos/DiscosIndex.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+
 
 const router = createBrowserRouter([
     { path: "/", element: <App /> },
     { path: "/sobre", element: <Sobre /> },
     { path: "/contato", element: <Contato /> },
-    { path: "/discos:id", element: <Discos/> },
+    { path: "/discos", element: <DiscosIndex/> },
     { path: "/discos/create", element: <DiscosCreate /> },
-    { path: "/discos:id/edit", element: <DiscosEdit /> },
+    { path: "/discos/:id", element: <DiscosShow/> },
+    { path: "/discos/:id/edit", element: <DiscosEdit /> },
 ]);
 createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
---
+++ src/pages/discos/DiscosIndex.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DiscosIndex = () => {
+  return (
+    <div>DiscosIndex</div>
+  )
+}
+
+export default DiscosIndex
